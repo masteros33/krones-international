fd71f6bb796a72013b49e85f2f73367c9b92a5ba
jude Obodai Sai <judeobodaisai@gmail.com>
2026-04-07 23:00:51 +0600

    Fiz standalone outpu mode

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  headers: async () => [
-    {
-      source: '/(.*)',
-      headers: [
-        { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
-        { key: 'Pragma', value: 'no-cache' },
-        { key: 'Expires', value: '0' },
-      ],
-    },
-  ],
+  output: 'standalone',
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'uniglobustrading.com' },
     ],
