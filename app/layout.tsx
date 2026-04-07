62d7d2b70451c60e9628dabd866d22eab23c3f56
jude Obodai Sai <judeobodaisai@gmail.com>
2026-04-07 23:09:07 +0600

    fix28

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
 
+export const dynamic = 'force-dynamic'
+
 export const metadata: Metadata = {
   title: 'Krones International Raw Materials Trading FZ LLC',
   description: 'Trusted Source for Raw Materials',
 }
-
-export const dynamic = 'force-dynamic'
 
 export default function RootLayout({
   children,
